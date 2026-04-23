2cdf61917ee507bb5be4766793522595072d3712
Ruoxuan Fu <ruoxuanfu@gmail.com>
2026-04-23 23:29:16 +0800

    Fix format issues with cargo fmt

--- native/src/completion.rs
+++ native/src/completion.rs
@@ -1,164 +1,164 @@
 #[derive(Debug, Clone)]
 pub struct CompletionItem {
-    pub label: String,
-    pub kind: CompletionItemKind,
-    pub documentation: Option<String>,
+  pub label: String,
+  pub kind: CompletionItemKind,
+  pub documentation: Option<String>,
 }
 
 #[derive(Debug, Clone, PartialEq, Eq)]
 pub enum CompletionItemKind {
-    Property,
-    EnumValue,
-    Type,
+  Property,
+  EnumValue,
+  Type,
 }
 
 pub struct CompletionEngine;
 
 impl CompletionEngine {
-    pub fn new() -> Self {
-        Self
-    }
+  pub fn new() -> Self {
+    Self
+  }
 
-    pub fn get_completions(&self, context: CompletionContext) -> Vec<CompletionItem> {
-        match context {
-            CompletionContext::MaterialBlock => self.get_material_properties(),
-            CompletionContext::ShadingModelValue => self.get_shading_model_values(),
-            CompletionContext::BlendingValue => self.get_blending_values(),
-            CompletionContext::ParameterType => self.get_parameter_types(),
-            CompletionContext::RequiresValue => self.get_requires_values(),
-        }
+  pub fn get_completions(&self, context: CompletionContext) -> Vec<CompletionItem> {
+    match context {
+      CompletionContext::MaterialBlock => self.get_material_properties(),
+      CompletionContext::ShadingModelValue => self.get_shading_model_values(),
+      CompletionContext::BlendingValue => self.get_blending_values(),
+      CompletionContext::ParameterType => self.get_parameter_types(),
+      CompletionContext::RequiresValue => self.get_requires_values(),
     }
+  }
 
-    fn get_material_properties(&self) -> Vec<CompletionItem> {
-        vec![
-            Self::property("name", "Material name identifier"),
-            Self::property("shadingModel", "Shading model (lit/unlit/subsurface/etc)"),
-            Self::property("requires", "Required vertex attributes"),
-            Self::property("parameters", "Material parameters list"),
-            Self::property("constants", "Material constants"),
-            Self::property("culling", "Face culling (front/back/none)"),
-            Self::property("blending", "Blending mode"),
-            Self::property("vertexDomain", "Vertex domain (object/world/view/device)"),
-            Self::property("doubleSided", "Whether material is two-sided"),
-            Self::property("colorWrite", "Enable color write"),
-            Self::property("depthWrite", "Enable depth write"),
-        ]
-    }
+  fn get_material_properties(&self) -> Vec<CompletionItem> {
+    vec![
+      Self::property("name", "Material name identifier"),
+      Self::property("shadingModel", "Shading model (lit/unlit/subsurface/etc)"),
+      Self::property("requires", "Required vertex attributes"),
+      Self::property("parameters", "Material parameters list"),
+      Self::property("constants", "Material constants"),
+      Self::property("culling", "Face culling (front/back/none)"),
+      Self::property("blending", "Blending mode"),
+      Self::property("vertexDomain", "Vertex domain (object/world/view/device)"),
+      Self::property("doubleSided", "Whether material is two-sided"),
+      Self::property("colorWrite", "Enable color write"),
+      Self::property("depthWrite", "Enable depth write"),
+    ]
+  }
 
-    fn get_shading_model_values(&self) -> Vec<CompletionItem> {
-        vec![
-            Self::enum_value("lit", "Standard PBR shading"),
-            Self::enum_value("unlit", "Unlit shading, no lighting"),
-            Self::enum_value("subsurface", "Subsurface scattering"),
-            Self::enum_value("cloth", "Cloth shading"),
-            Self::enum_value("specularGlossiness", "Specular-glossiness workflow"),
-        ]
-    }
+  fn get_shading_model_values(&self) -> Vec<CompletionItem> {
+    vec![
+      Self::enum_value("lit", "Standard PBR shading"),
+      Self::enum_value("unlit", "Unlit shading, no lighting"),
+      Self::enum_value("subsurface", "Subsurface scattering"),
+      Self::enum_value("cloth", "Cloth shading"),
+      Self::enum_value("specularGlossiness", "Specular-glossiness workflow"),
+    ]
+  }
 
-    fn get_blending_values(&self) -> Vec<CompletionItem> {
-        vec![
-            Self::enum_value("opaque", "Opaque blending"),
-            Self::enum_value("transparent", "Alpha blending"),
-            Self::enum_value("fade", "Fade transparency"),
-            Self::enum_value("masked", "Alpha mask (binary)"),
-            Self::enum_value("add", "Additive blending"),
-            Self::enum_value("custom", "Custom blending"),
-        ]
-    }
+  fn get_blending_values(&self) -> Vec<CompletionItem> {
+    vec![
+      Self::enum_value("opaque", "Opaque blending"),
+      Self::enum_value("transparent", "Alpha blending"),
+      Self::enum_value("fade", "Fade transparency"),
+      Self::enum_value("masked", "Alpha mask (binary)"),
+      Self::enum_value("add", "Additive blending"),
+      Self::enum_value("custom", "Custom blending"),
+    ]
+  }
 
-    fn get_parameter_types(&self) -> Vec<CompletionItem> {
-        vec![
-            Self::param_type("bool", "Boolean value"),
-            Self::param_type("bool2", "2-component boolean vector"),
-            Self::param_type("bool3", "3-component boolean vector"),
-            Self::param_type("bool4", "4-component boolean vector"),
-            Self::param_type("int", "Integer value"),
-            Self::param_type("int2", "2-component integer vector"),
-            Self::param_type("int3", "3-component integer vector"),
-            Self::param_type("int4", "4-component integer vector"),
-            Self::param_type("uint", "Unsigned integer"),
-            Self::param_type("uint2", "2-component uint vector"),
-            Self::param_type("uint3", "3-component uint vector"),
-            Self::param_type("uint4", "4-component uint vector"),
-            Self::param_type("float", "Floating point value"),
-            Self::param_type("float2", "2-component float vector"),
-            Self::param_type("float3", "3-component float vector"),
-            Self::param_type("float4", "4-component float vector"),
-            Self::param_type("mat3", "3x3 matrix"),
-            Self::param_type("mat4", "4x4 matrix"),
-            Self::param_type("sampler2d", "2D texture sampler"),
-            Self::param_type("sampler3d", "3D texture sampler"),
-            Self::param_type("samplerCubemap", "Cube map sampler"),
-            Self::param_type("samplerExternal", "External image sampler"),
-        ]
-    }
+  fn get_parameter_types(&self) -> Vec<CompletionItem> {
+    vec![
+      Self::param_type("bool", "Boolean value"),
+      Self::param_type("bool2", "2-component boolean vector"),
+      Self::param_type("bool3", "3-component boolean vector"),
+      Self::param_type("bool4", "4-component boolean vector"),
+      Self::param_type("int", "Integer value"),
+      Self::param_type("int2", "2-component integer vector"),
+      Self::param_type("int3", "3-component integer vector"),
+      Self::param_type("int4", "4-component integer vector"),
+      Self::param_type("uint", "Unsigned integer"),
+      Self::param_type("uint2", "2-component uint vector"),
+      Self::param_type("uint3", "3-component uint vector"),
+      Self::param_type("uint4", "4-component uint vector"),
+      Self::param_type("float", "Floating point value"),
+      Self::param_type("float2", "2-component float vector"),
+      Self::param_type("float3", "3-component float vector"),
+      Self::param_type("float4", "4-component float vector"),
+      Self::param_type("mat3", "3x3 matrix"),
+      Self::param_type("mat4", "4x4 matrix"),
+      Self::param_type("sampler2d", "2D texture sampler"),
+      Self::param_type("sampler3d", "3D texture sampler"),
+      Self::param_type("samplerCubemap", "Cube map sampler"),
+      Self::param_type("samplerExternal", "External image sampler"),
+    ]
+  }
 
-    fn get_requires_values(&self) -> Vec<CompletionItem> {
-        vec![
-            Self::enum_value("position", "Vertex position"),
-            Self::enum_value("normal", "Vertex normal"),
-            Self::enum_value("uv0", "UV coordinate set 0"),
-            Self::enum_value("uv1", "UV coordinate set 1"),
-            Self::enum_value("color", "Vertex color"),
-            Self::enum_value("tangents", "Tangent and bitangent"),
-            Self::enum_value("custom0", "Custom attribute 0"),
-            Self::enum_value("custom1", "Custom attribute 1"),
-            Self::enum_value("custom2", "Custom attribute 2"),
-            Self::enum_value("custom3", "Custom attribute 3"),
-            Self::enum_value("custom4", "Custom attribute 4"),
-            Self::enum_value("boneIndices", "Bone indices for skinning"),
-            Self::enum_value("boneWeights", "Bone weights for skinning"),
-        ]
-    }
+  fn get_requires_values(&self) -> Vec<CompletionItem> {
+    vec![
+      Self::enum_value("position", "Vertex position"),
+      Self::enum_value("normal", "Vertex normal"),
+      Self::enum_value("uv0", "UV coordinate set 0"),
+      Self::enum_value("uv1", "UV coordinate set 1"),
+      Self::enum_value("color", "Vertex color"),
+      Self::enum_value("tangents", "Tangent and bitangent"),
+      Self::enum_value("custom0", "Custom attribute 0"),
+      Self::enum_value("custom1", "Custom attribute 1"),
+      Self::enum_value("custom2", "Custom attribute 2"),
+      Self::enum_value("custom3", "Custom attribute 3"),
+      Self::enum_value("custom4", "Custom attribute 4"),
+      Self::enum_value("boneIndices", "Bone indices for skinning"),
+      Self::enum_value("boneWeights", "Bone weights for skinning"),
+    ]
+  }
 
-    fn property(label: &str, docs: &str) -> CompletionItem {
-        CompletionItem {
-            label: label.to_string(),
-            kind: CompletionItemKind::Property,
-            documentation: Some(docs.to_string()),
-        }
+  fn property(label: &str, docs: &str) -> CompletionItem {
+    CompletionItem {
+      label: label.to_string(),
+      kind: CompletionItemKind::Property,
+      documentation: Some(docs.to_string()),
     }
+  }
 
-    fn enum_value(label: &str, docs: &str) -> CompletionItem {
-        CompletionItem {
-            label: label.to_string(),
-            kind: CompletionItemKind::EnumValue,
-            documentation: Some(docs.to_string()),
-        }
+  fn enum_value(label: &str, docs: &str) -> CompletionItem {
+    CompletionItem {
+      label: label.to_string(),
+      kind: CompletionItemKind::EnumValue,
+      documentation: Some(docs.to_string()),
     }
+  }
 
-    fn param_type(label: &str, docs: &str) -> CompletionItem {
-        CompletionItem {
-            label: label.to_string(),
-            kind: CompletionItemKind::Type,
-            documentation: Some(docs.to_string()),
-        }
+  fn param_type(label: &str, docs: &str) -> CompletionItem {
+    CompletionItem {
+      label: label.to_string(),
+      kind: CompletionItemKind::Type,
+      documentation: Some(docs.to_string()),
     }
+  }
 }
 
 #[derive(Debug, Clone, Copy, PartialEq, Eq)]
 pub enum CompletionContext {
-    MaterialBlock,
-    ShadingModelValue,
-    BlendingValue,
-    ParameterType,
-    RequiresValue,
+  MaterialBlock,
+  ShadingModelValue,
+  BlendingValue,
+  ParameterType,
+  RequiresValue,
 }
 
 impl Default for CompletionEngine {
-    fn default() -> Self {
-        Self::new()
-    }
+  fn default() -> Self {
+    Self::new()
+  }
 }
 
 #[cfg(test)]
 mod tests {
-    use super::*;
+  use super::*;
 
-    #[test]
-    fn test_get_material_properties() {
-        let engine = CompletionEngine::new();
-        let completions = engine.get_completions(CompletionContext::MaterialBlock);
-        assert!(!completions.is_empty());
-    }
+  #[test]
+  fn test_get_material_properties() {
+    let engine = CompletionEngine::new();
+    let completions = engine.get_completions(CompletionContext::MaterialBlock);
+    assert!(!completions.is_empty());
+  }
 }
--- native/src/token.rs
+++ native/src/token.rs
@@ -3,195 +3,195 @@ use serde::Serialize;
 
 #[derive(Debug, Clone, PartialEq, Serialize)]
 pub enum TokenType {
-    // Top-level blocks
-    Material,
-    Vertex,
-    Fragment,
-    Compute,
+  // Top-level blocks
+  Material,
+  Vertex,
+  Fragment,
+  Compute,
 
-    // Material properties
-    Name,
-    ShadingModel,
-    Requires,
-    Culling,
-    Blending,
-    MaskThreshold,
-    AlphaToCoverage,
-    VertexDomain,
-    MaterialDomain,
-    Interpolation,
-    DoubleSided,
-    ColorWrite,
-    DepthWrite,
-    DepthTest,
-    Instanced,
-    RefractionMode,
-    RefractionType,
-    ReflectionMode,
-    TransparencyMode,
-    ShadowMultiplier,
-    SpecularAntiAliasing,
-    SpecularAntiAliasingVariance,
-    SpecularAntiAliasingThreshold,
-    ClearCoatIorChange,
-    FlipUv,
-    LinearFog,
-    MultiBounceAmbientOcclusion,
-    SpecularAmbientOcclusion,
-    CustomSurfaceShading,
-    StereoscopicType,
-    StereoscopicEyeCount,
-    GroupSize,
-    VariantFilter,
-    Parameters,
-    Constants,
-    Variables,
+  // Material properties
+  Name,
+  ShadingModel,
+  Requires,
+  Culling,
+  Blending,
+  MaskThreshold,
+  AlphaToCoverage,
+  VertexDomain,
+  MaterialDomain,
+  Interpolation,
+  DoubleSided,
+  ColorWrite,
+  DepthWrite,
+  DepthTest,
+  Instanced,
+  RefractionMode,
+  RefractionType,
+  ReflectionMode,
+  TransparencyMode,
+  ShadowMultiplier,
+  SpecularAntiAliasing,
+  SpecularAntiAliasingVariance,
+  SpecularAntiAliasingThreshold,
+  ClearCoatIorChange,
+  FlipUv,
+  LinearFog,
+  MultiBounceAmbientOcclusion,
+  SpecularAmbientOcclusion,
+  CustomSurfaceShading,
+  StereoscopicType,
+  StereoscopicEyeCount,
+  GroupSize,
+  VariantFilter,
+  Parameters,
+  Constants,
+  Variables,
 
-    // Property names
-    Type,
-    Precision,
-    Format,
-    Filterable,
-    Multisample,
-    TransformName,
-    Stages,
-    Default,
+  // Property names
+  Type,
+  Precision,
+  Format,
+  Filterable,
+  Multisample,
+  TransformName,
+  Stages,
+  Default,
 
-    // Enums - shading model
-    Lit,
-    Unlit,
-    Subsurface,
-    Cloth,
-    SpecularGlossiness,
+  // Enums - shading model
+  Lit,
+  Unlit,
+  Subsurface,
+  Cloth,
+  SpecularGlossiness,
 
-    // Enums - culling
-    Front,
-    Back,
-    None,
+  // Enums - culling
+  Front,
+  Back,
+  None,
 
-    // Enums - blending
-    Opaque,
-    Transparent,
-    Fade,
-    Masked,
-    Add,
-    Custom,
+  // Enums - blending
+  Opaque,
+  Transparent,
+  Fade,
+  Masked,
+  Add,
+  Custom,
 
-    // Enums - vertex domain
-    Object,
-    World,
-    View,
-    Device,
+  // Enums - vertex domain
+  Object,
+  World,
+  View,
+  Device,
 
-    // Enums - material domain
-    Surface,
-    PostProcess,
+  // Enums - material domain
+  Surface,
+  PostProcess,
 
-    // Enums - interpolation
-    Smooth,
-    Flat,
+  // Enums - interpolation
+  Smooth,
+  Flat,
 
-    // Enums - refraction mode
-    ScreenSpace,
-    Cubemap,
+  // Enums - refraction mode
+  ScreenSpace,
+  Cubemap,
 
-    // Enums - refraction type
-    Solid,
-    Thin,
+  // Enums - refraction type
+  Solid,
+  Thin,
 
-    // Enums - transparency mode
-    TwoPassesOneSide,
-    TwoPassesTwoSides,
+  // Enums - transparency mode
+  TwoPassesOneSide,
+  TwoPassesTwoSides,
 
-    // Enums - vertex attributes
-    Position,
-    Normal,
-    Uv0,
-    Uv1,
-    Color,
-    Tangents,
-    Custom0,
-    Custom1,
-    Custom2,
-    Custom3,
-    Custom4,
-    BoneIndices,
-    BoneWeights,
+  // Enums - vertex attributes
+  Position,
+  Normal,
+  Uv0,
+  Uv1,
+  Color,
+  Tangents,
+  Custom0,
+  Custom1,
+  Custom2,
+  Custom3,
+  Custom4,
+  BoneIndices,
+  BoneWeights,
 
-    // Parameter types
-    Bool,
-    Bool2,
-    Bool3,
-    Bool4,
-    Int,
-    Int2,
-    Int3,
-    Int4,
-    Uint,
-    Uint2,
-    Uint3,
-    Uint4,
-    Float,
-    Float2,
-    Float3,
-    Float4,
-    Mat3,
-    Mat4,
-    Sampler2d,
-    Sampler3d,
-    SamplerCubemap,
-    SamplerExternal,
+  // Parameter types
+  Bool,
+  Bool2,
+  Bool3,
+  Bool4,
+  Int,
+  Int2,
+  Int3,
+  Int4,
+  Uint,
+  Uint2,
+  Uint3,
+  Uint4,
+  Float,
+  Float2,
+  Float3,
+  Float4,
+  Mat3,
+  Mat4,
+  Sampler2d,
+  Sampler3d,
+  SamplerCubemap,
+  SamplerExternal,
 
-    // Literals
-    True,
-    False,
-    Null,
-    Number,
-    String,
+  // Literals
+  True,
+  False,
+  Null,
+  Number,
+  String,
 
-    // Punctuation
-    LCurly,
-    RCurly,
-    LBracket,
-    RBracket,
-    Colon,
-    Comma,
+  // Punctuation
+  LCurly,
+  RCurly,
+  LBracket,
+  RBracket,
+  Colon,
+  Comma,
 
-    // Other
-    Identifier,
-    Comment,
-    GlslCode,
-    Whitespace,
-    Unknown,
+  // Other
+  Identifier,
+  Comment,
+  GlslCode,
+  Whitespace,
+  Unknown,
 }
 
 #[napi(object)]
 #[derive(Debug, Clone, PartialEq, Serialize)]
 pub struct Token {
-    pub token_type: String,
-    pub value: String,
-    pub line: u32,
-    pub column: u32,
+  pub token_type: String,
+  pub value: String,
+  pub line: u32,
+  pub column: u32,
 }
 
 impl Token {
-    pub fn new(token_type: TokenType, value: &str, line: u32, column: u32) -> Self {
-        Self {
-            token_type: format!("{token_type:?}"),
-            value: value.to_string(),
-            line,
-            column,
-        }
+  pub fn new(token_type: TokenType, value: &str, line: u32, column: u32) -> Self {
+    Self {
+      token_type: format!("{token_type:?}"),
+      value: value.to_string(),
+      line,
+      column,
     }
+  }
 }
 
 // Helper trait to make working with tokens easier
 pub trait TokenExt {
-    fn is_type(&self, token_type: &TokenType) -> bool;
+  fn is_type(&self, token_type: &TokenType) -> bool;
 }
 
 impl TokenExt for Token {
-    fn is_type(&self, token_type: &TokenType) -> bool {
-        self.token_type == format!("{token_type:?}")
-    }
+  fn is_type(&self, token_type: &TokenType) -> bool {
+    self.token_type == format!("{token_type:?}")
+  }
 }
